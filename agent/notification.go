7dc2de7dfbfd982366425ce9f651e0b44dd1b68a
Thomas Glachant <thomas@glachant.fr>
2022-09-21 16:05:20 +0200

    fix: apply gofmt

--- agent/notification.go
+++ agent/notification.go
@@ -1,74 +1,74 @@
 package agent
 
 import (
-    "fmt"
+	"fmt"
 
-    "gobana-agent/core"
+	"gobana-agent/core"
 )
 
 const (
-    notifierLogPrefix            = "notification"
-    subscriptionTypeEmail        = "email"
-    subscriptionTypeSlackWebhook = "slack_webhook"
+	notifierLogPrefix            = "notification"
+	subscriptionTypeEmail        = "email"
+	subscriptionTypeSlackWebhook = "slack_webhook"
 )
 
 type Notification interface {
-    Subject() string
-    TemplateName() string
-    Data() map[string]interface{}
+	Subject() string
+	TemplateName() string
+	Data() map[string]interface{}
 }
 
 func SendNotification(notification Notification) error {
-    for _, recipient := range AppConfig.Alerts.Recipients {
-        if recipient.Kind == subscriptionTypeEmail {
-            if err := sendEmail(recipient.Recipient, notification); err != nil {
-                return fmt.Errorf("error sending email: %s", err.Error())
-            }
-        } else if recipient.Kind == subscriptionTypeSlackWebhook {
-            if err := sendSlack(recipient.Recipient, notification); err != nil {
-                return fmt.Errorf("error sending email: %s", err.Error())
-            }
-        }
-    }
-    return nil
+	for _, recipient := range AppConfig.Alerts.Recipients {
+		if recipient.Kind == subscriptionTypeEmail {
+			if err := sendEmail(recipient.Recipient, notification); err != nil {
+				return fmt.Errorf("error sending email: %s", err.Error())
+			}
+		} else if recipient.Kind == subscriptionTypeSlackWebhook {
+			if err := sendSlack(recipient.Recipient, notification); err != nil {
+				return fmt.Errorf("error sending email: %s", err.Error())
+			}
+		}
+	}
+	return nil
 }
 
 func sendEmail(email string, notification Notification) error {
-    core.Logger.Infof(notifierLogPrefix, "Send an email")
+	core.Logger.Infof(notifierLogPrefix, "Send an email")
 
-    err := core.SendEmail(
-        &AppConfig.SMTP,
-        email,
-        notification.Subject(),
-        notification.TemplateName(),
-        notification.Data(),
-    )
-    if err != nil {
-        return err
-    }
-    return nil
+	err := core.SendEmail(
+		&AppConfig.SMTP,
+		email,
+		notification.Subject(),
+		notification.TemplateName(),
+		notification.Data(),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func sendSlack(webhookURL string, notification Notification) error {
-    // check if template exists
-    templateFile := fmt.Sprintf("templates/slack/%s.txt.tmpl", notification.TemplateName())
-    if !core.CheckTemplateExists(templateFile) {
-        return fmt.Errorf("template file \"%s\" not found", templateFile)
-    }
+	// check if template exists
+	templateFile := fmt.Sprintf("templates/slack/%s.txt.tmpl", notification.TemplateName())
+	if !core.CheckTemplateExists(templateFile) {
+		return fmt.Errorf("template file \"%s\" not found", templateFile)
+	}
 
-    core.Logger.Infof(notifierLogPrefix, "Send a slack message")
+	core.Logger.Infof(notifierLogPrefix, "Send a slack message")
 
-    err := core.SendSlackNotification(
-        webhookURL,
-        core.SlackRequest{
-            Text: core.TemplateToString(
-                []string{templateFile},
-                notification.Data(),
-            ),
-        },
-    )
-    if err != nil {
-        return err
-    }
-    return nil
+	err := core.SendSlackNotification(
+		webhookURL,
+		core.SlackRequest{
+			Text: core.TemplateToString(
+				[]string{templateFile},
+				notification.Data(),
+			),
+		},
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
